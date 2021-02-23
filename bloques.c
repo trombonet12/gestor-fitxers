7b0a666258f7a45eb0ebbb1e73d3be9b373ddaad
Miquel Vidal <miquelvidalcortes@gmail.com>
2021-02-23 12:58:57 +0100

    nivell 1 comentat

--- bloques.c
+++ bloques.c
@@ -1,19 +1,22 @@
 //AUTORS: Joan López Ferrer i Miquel Vidal Cortés
 
 #include "bloques.h"
+//Declaram i inicialitzam una variable int que contindrà el valor del descriptor.
 static int descriptor = 0;
 
 //Inicialitzar dispositiu virtual.
 int bmount(const char *camino)
 {
+    //Establim l'enlaç amb el fitxer passat per paràmetre, aplicant els permisos i els flags adiets.
     descriptor = open(camino, O_RDWR | O_CREAT, 0666);
     if (descriptor == -1)
     {
-        //Error.
+        //Cas operació errònia.
         return EXIT_FAILURE;
     }
     else
     {
+        //Cas operació completada amb èxit.
         return descriptor;
     }
 }
@@ -21,24 +24,27 @@ int bmount(const char *camino)
 //Tancam enllaç amb el dispositiu virtual.
 int bumount()
 {
-    //Retora EXIT_SUCCESS o EXIT_FAILURE.
+    //Retora EXIT_SUCCESS (Cas operació completada correctament) o EXIT_FAILURE(Cas operació errònia).
     return close(descriptor);
 }
 
 //Escriptura d'un bloc al dispositiu virtual.
 int bwrite(unsigned int nbloque, const void *buf)
 {
+    //Declaram varibale int que conté el nombre de bytes que hem escrit al dispositiu virtual per cada crida al mètode.
     int numBytes = 0;
-    //Calculam el desplaçament.
+    //Calculam el desplaçament (lloc exacte on escriure el bloc).
     int desplazamineto = nbloque * BLOCKSIZE;
-
+    //Posiciona el punter al lloc exacte.
     lseek(descriptor,desplazamineto,SEEK_SET);
-
+    //Duim a terme l'escriptura d'un bloc al dispositiu virutal.
     numBytes = write(descriptor,buf,BLOCKSIZE);
-
+    //Comprovam el resultat de l'operació.
     if (numBytes<0){
+        //Cas operació errònia.
         return EXIT_FAILURE;
     }else{
+         //Cas operació completada amb èxit.
         return numBytes;
     }
 
@@ -47,17 +53,20 @@ int bwrite(unsigned int nbloque, const void *buf)
 //Lectura d'un bloc des del dispositiu virtual.
 int bread(unsigned int nbloque, void *buf)
 {
+    //Declaram varibale int que conté el nombre de bytes que hem llegit des del dispositiu virtual per cada crida al mètode.
     int numBytes = 0;
-    //Calculam el desplaçament.
+    //Calculam el desplaçament (lloc exacte on llegirem el bloc).
     int desplazamineto = nbloque * BLOCKSIZE;
-
+    //Posiciona el punter al lloc exacte.
     lseek(descriptor,desplazamineto,SEEK_SET);
-
+    //Duim a terme la lectura d'un bloc al dispositiu virutal.
     numBytes = read(descriptor,buf,BLOCKSIZE);
-
+    //Comprovam el resultat de l'operació.
     if (numBytes<0){
+        //Cas operació errònia.
         return EXIT_FAILURE;
     }else{
+        //Cas operació completada amb èxit.
         return numBytes;
     }
 }
--- mi_mkfs.c
+++ mi_mkfs.c
@@ -1,31 +1,39 @@
 //AUTORS: Joan López Ferrer i Miquel Vidal Cortés
 
 #include "bloques.h"
-
+//Declaram una varibale que contendrà el contigut a llegir o escriure. REALEMNT QUE FA
 unsigned char *buffer[BLOCKSIZE];
 
+//Main del programa.
 int main(int argc, char **argv)
 {
+    //Establim enllaç amb el dispositiu virtual.
     if (bmount(argv[1]))
     {
-        printf("Operació realitzada correctamnet");
+        printf("Operació realitzada correctament \n");
     }
     else
     {
+        //Control d'errors.
         fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
     }
+    //Inicialitzam a 0 totes les posicions del buffer.
     memset(buffer, 0, BLOCKSIZE);
+    //Recorrem tots els blocs.
     for (int i = 0; i < atoi(argv[2]); i++)
     {
-
+        //Escriputra del bloc i-èssim.
         if (bwrite(i, buffer) < 0)
         {
+            //Control d'erros.
             fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
         }
     }
 
-    if(bumount()<0){
-        
+    //Tancam l'enllaç amb el dispositiu virutal.
+    if (bumount() < 0)
+    {
+        //Control d'erros.
+        fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
     }
-    
 }
