3deff3246249ff5ac3561fa03f060ba6ccc6fccb
Joan López <trombonet12@gmail.com>
2021-02-23 11:19:45 +0100

    nivell1 provisional

--- bloques.c
+++ bloques.c
@@ -1,4 +1,63 @@
 //AUTORS: Joan López Ferrer i Miquel Vidal Cortés
 
 #include "bloques.h"
+static int descriptor = 0;
 
+//Inicialitzar dispositiu virtual.
+int bmount(const char *camino)
+{
+    descriptor = open(camino, O_RDWR | O_CREAT, 0666);
+    if (descriptor == -1)
+    {
+        //Error.
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        return descriptor;
+    }
+}
+
+//Tancam enllaç amb el dispositiu virtual.
+int bumount()
+{
+    //Retora EXIT_SUCCESS o EXIT_FAILURE.
+    return close(descriptor);
+}
+
+//Escriptura d'un bloc al dispositiu virtual.
+int bwrite(unsigned int nbloque, const void *buf)
+{
+    int numBytes = 0;
+    //Calculam el desplaçament.
+    int desplazamineto = nbloque * BLOCKSIZE;
+
+    lseek(descriptor,desplazamineto,SEEK_SET);
+
+    numBytes = write(descriptor,buf,BLOCKSIZE);
+
+    if (numBytes<0){
+        return EXIT_FAILURE;
+    }else{
+        return numBytes;
+    }
+
+}
+
+//Lectura d'un bloc des del dispositiu virtual.
+int bread(unsigned int nbloque, void *buf)
+{
+    int numBytes = 0;
+    //Calculam el desplaçament.
+    int desplazamineto = nbloque * BLOCKSIZE;
+
+    lseek(descriptor,desplazamineto,SEEK_SET);
+
+    numBytes = read(descriptor,buf,BLOCKSIZE);
+
+    if (numBytes<0){
+        return EXIT_FAILURE;
+    }else{
+        return numBytes;
+    }
+}
--- mi_mkfs.c
+++ mi_mkfs.c
@@ -2,3 +2,30 @@
 
 #include "bloques.h"
 
+unsigned char *buffer[BLOCKSIZE];
+
+int main(int argc, char **argv)
+{
+    if (bmount(argv[1]))
+    {
+        printf("Operació realitzada correctamnet");
+    }
+    else
+    {
+        fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
+    }
+    memset(buffer, 0, BLOCKSIZE);
+    for (int i = 0; i < atoi(argv[2]); i++)
+    {
+
+        if (bwrite(i, buffer) < 0)
+        {
+            fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
+        }
+    }
+
+    if(bumount()<0){
+        
+    }
+    
+}
