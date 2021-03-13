bd5f8f466333bfc0a7aee256bf76a01561e6d9c5
Miquel_Vidal <miquelvidalcortes@gmail.com>
2021-03-13 12:44:37 +0100

    nivell dos practicament acabat

--- mi_mkfs.c
+++ mi_mkfs.c
@@ -34,18 +34,6 @@ int main(int argc, char **argv)
     initSB(atoi(argv[2]),atoi(argv[2])/4);
     initMB();
     initAI();
-
-/*
-    struct superbloque *SB = (struct superbloque *)malloc(sizeof(struct superbloque));
-    bread(posSB, SB);
-    printf("Soc a nes mian comprovant tamany de SB \n");
-    printf("Tamany de SB: %lu \n", sizeof(*SB)); 
-    printf("Tamany de SB: %lu \n", sizeof(struct superbloque)); //Print clarificatiu
-    
-    struct inodo inodos [BLOCKSIZE/INODOSIZE];
-    printf("Tamany de INODE: %lu \n", sizeof(struct inodo)); //Print clarificatiu
-
-*/
     //Tancam l'enllaç amb el dispositiu virutal.
     if (bumount() < 0)
     {
@@ -53,6 +41,8 @@ int main(int argc, char **argv)
         fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
     }
    
+    
+   // initSB(atoi(argv[2]),atoi(argv[2])/4);
 }
 
 
