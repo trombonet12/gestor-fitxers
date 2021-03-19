66b6d4525cc2ed0a6d32ab88bde151fcce7a7b12
Miquel_Vidal <miquelvidalcortes@gmail.com>
2021-03-19 19:05:04 +0100

    comprovacions funcions fetes (5) son correctes

--- mi_mkfs.c
+++ mi_mkfs.c
@@ -33,14 +33,24 @@ int main(int argc, char **argv)
     tamAI(atoi(argv[2])/4);
     initSB(atoi(argv[2]),atoi(argv[2])/4);
     initMB();
+   //ponerAUnoBits();
     initAI();
-   // int bloqueAux = reservar_bloque();
-   // printf("Reservar primer bloque libre: %d",bloqueAux);
-   // liberar_bloque(bloqueAux);
+    //int bloqueAux = reservar_bloque();
+    //printf("Reservar primer bloque libre: %d",bloqueAux);
+    //liberar_bloque(bloqueAux);
     
 
-    //escribir_bit(40003,1);
-    //printf ("El bit 0 vale: %d ",leer_bit(40003));
+
+    printf ("El bit 0 vale: %d \n",leer_bit(0));
+    printf ("El bit 1 vale: %d \n",leer_bit(1));
+    printf ("El bit 13 vale: %d ",leer_bit(13));
+    printf ("El bit 14 vale: %d ",leer_bit(14));
+    printf ("El bit 3138 vale: %d ",leer_bit(3138));
+    printf ("El bit 3139 vale: %d ",leer_bit(3139));
+    printf ("El bit 99999 vale: %d ",leer_bit(99999));
+
+
+
     //Tancam l'enllaç amb el dispositiu virutal.
     if (bumount() < 0)
     {
