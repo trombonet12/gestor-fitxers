63389f84189c70c600b716cf681ca333f8c61487
Joan López Ferrer <61268006+trombonet12@users.noreply.github.com>
2021-03-21 18:34:45 +0100

    Update ficheros_basico.c

--- ficheros_basico.c
+++ ficheros_basico.c
@@ -156,7 +156,7 @@ void ponerAUnoBits()
     printf("\n");
 }
 
-int initMB(unsigned int nbloques)
+int initMB()
 {
     printf("DINS initMB");
     //Declaracio de les variables
@@ -191,13 +191,13 @@ int initMB(unsigned int nbloques)
     printf("Num de blocs escrits: %d\n", blocs); //Print Clarificatiu
 
     //Posar a 1 el bits corresponents als blocs dels Metadatos.
-    int aux = tamSB + tamMB(nbloques) + tamAI(SB.totInodos);
+    int aux = tamSB + tamMB(SB.totBloques) + tamAI(SB.totInodos);
     for (int i = 0; i < aux; i++)
     {
         escribir_bit(i, 1);
     }
     //Actualització de la quantitat de bloc lliures.
-    SB.cantBloquesLibres = SB.cantBloquesLibres - (tamSB + tamMB(nbloques) + tamAI(SB.totInodos));
+    SB.cantBloquesLibres = SB.cantBloquesLibres - (tamSB + tamMB(SB.totBloques) + tamAI(SB.totInodos));
     printf("Cantidad de bloques libres: %d", SB.cantBloquesLibres);
     //Salvaguardam el SuperBloc dins el Dispositiu Virtual.
     if (bwrite(posSB, &SB) == BLOCKSIZE)
