05860976c12da50396288f87ef2c91d3aa0a4ecc
Joan Lopez Ferrer <trombonet12@gmail.com>
2021-05-05 18:45:27 +0200

    buscar_entrada() optimitzat

--- leer_sf.c
+++ leer_sf.c
@@ -2,7 +2,7 @@
 
 #include "directorios.h"
 
- /*
+/*
 void mostrar_buscar_entrada(char *camino, char reservar){
   unsigned int p_inodo_dir = 0;
   unsigned int p_inodo = 0;
@@ -19,33 +19,33 @@ void mostrar_buscar_entrada(char *camino, char reservar){
 
 int main(int argc, char **argv)
 {
-    //Establim enllaç amb el dispositiu virtual.
-    if (bmount(argv[1]) == ERROR)
-    {
-        //Control d'errors.
-        fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
-    }
+  //Establim enllaç amb el dispositiu virtual.
+  if (bmount(argv[1]) == ERROR)
+  {
+    //Control d'errors.
+    fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
+  }
 
-    struct superbloque SB;
-    //Llegim el SuperBloc
-    if (bread(posSB, &SB) == ERROR)
-    {
-        //Error en la lectura.
-        fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
-    }
+  struct superbloque SB;
+  //Llegim el SuperBloc
+  if (bread(posSB, &SB) == ERROR)
+  {
+    //Error en la lectura.
+    fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
+  }
 
-    printf("El primer bloque de MB és: %d \n", SB.posPrimerBloqueMB);                        //Print clarificatiu
-    printf("El darrer bloc del MB és: %d \n", SB.posUltimoBloqueMB);                         //Print clarificatiu
-    printf("El primer bloc de l'array inodos és: %d \n", SB.posPrimerBloqueAI);              //Print clarificatiu
-    printf("El darrer bloc de l'array inodos és: %d \n", SB.posUltimoBloqueAI);              //Print clarificatiu
-    printf("El primer bloc de Datos és: %d \n", SB.posPrimerBloqueDatos);                    //Print clarificatiu
-    printf("El darrer bloc de Datoes és: %d \n", SB.posUltimoBloqueDatos);                   //Print clarificatiu
-    printf("La posició del inode arrel en l'array inodes és: %d \n", SB.posInodoRaiz);       //Print clarificatiu
-    printf("El primer inode lliure dins l'arrar inodes és: %d \n", SB.posPrimerInodoLibre);  //Print clarificatiu
-    printf("Quantitat de blocs lliures : %d \n", SB.cantBloquesLibres);                      //Print clarificatiu
-    printf("Quantitat de inodes lliures al array de inodes és: %d \n", SB.cantInodosLibres); //Print clarificatiu
-    printf("Quantitat de blocs totals és: %d \n", SB.totBloques);                            //Print clarificatiu
-    printf("Quantitat de inodes és: %d \n\n", SB.totInodos);                                 //Print clarificatiu
+  printf("El primer bloque de MB és: %d \n", SB.posPrimerBloqueMB);                        //Print clarificatiu
+  printf("El darrer bloc del MB és: %d \n", SB.posUltimoBloqueMB);                         //Print clarificatiu
+  printf("El primer bloc de l'array inodos és: %d \n", SB.posPrimerBloqueAI);              //Print clarificatiu
+  printf("El darrer bloc de l'array inodos és: %d \n", SB.posUltimoBloqueAI);              //Print clarificatiu
+  printf("El primer bloc de Datos és: %d \n", SB.posPrimerBloqueDatos);                    //Print clarificatiu
+  printf("El darrer bloc de Datoes és: %d \n", SB.posUltimoBloqueDatos);                   //Print clarificatiu
+  printf("La posició del inode arrel en l'array inodes és: %d \n", SB.posInodoRaiz);       //Print clarificatiu
+  printf("El primer inode lliure dins l'arrar inodes és: %d \n", SB.posPrimerInodoLibre);  //Print clarificatiu
+  printf("Quantitat de blocs lliures : %d \n", SB.cantBloquesLibres);                      //Print clarificatiu
+  printf("Quantitat de inodes lliures al array de inodes és: %d \n", SB.cantInodosLibres); //Print clarificatiu
+  printf("Quantitat de blocs totals és: %d \n", SB.totBloques);                            //Print clarificatiu
+  printf("Quantitat de inodes és: %d \n\n", SB.totInodos);                                 //Print clarificatiu
 
   /* 
   mostrar_buscar_entrada("pruebas/", 1); //ERROR_CAMINO_INCORRECTO
@@ -62,12 +62,10 @@ int main(int argc, char **argv)
   mostrar_buscar_entrada("/pruebas/docs/doc2", 1); //creamos /pruebas/docs/doc2
   */
 
-
-
-    //Tancam l'enllaç amb el dispositiu virutal.
-    if (bumount() < 0)
-    {
-        //Control d'erros.
-        fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
-    }
+  //Tancam l'enllaç amb el dispositiu virutal.
+  if (bumount() < 0)
+  {
+    //Control d'erros.
+    fprintf(stderr, "Error %d: %s\n", errno, strerror(errno));
+  }
 }
